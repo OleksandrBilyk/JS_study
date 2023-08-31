a63a0964ccfaa31048b81d473b07557d796edd5d
Oleksandr <biluksanya@ukr.net>
2023-08-31 09:36:05 +0300

    lesson4 done+ lesson3 error fixed

--- Lesson_3/lesson_3_array_task1.js
+++ Lesson_3/lesson_3_array_task1.js
@@ -1,8 +1,10 @@
 {
     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+    document.write(`<ul>`)
     for (const listOfItem of listOfItems) {
         document.write(`<li>
                            ${listOfItem}
                       </li>`);
     }
+    document.write(`</ul>`)
 }
